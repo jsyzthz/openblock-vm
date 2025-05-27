fe8f85511c879b2c004a7e7030c0c54ad4f50ff0
Tong Huazhong <jsyzthz@gmail.com>
2025-05-28 01:20:18 +0800

    更新内置device.

---
+++ src/devices/scratchstar/scratchstar.js
@@ -0,0 +1,102 @@
+const formatMessage = require('format-message');
+
+const ArduinoPeripheral = require('../common/arduino-peripheral');
+
+/**
+ * The list of USB device filters.
+ * @readonly
+ */
+const PNPID_LIST = [
+    // https://github.com/arduino/Arduino/blob/1.8.0/hardware/arduino/avr/boards.txt#L51-L58
+    'USB\\VID_2341&PID_0010',
+    'USB\\VID_2341&PID_0042',
+    'USB\\VID_2A03&PID_0010',
+    'USB\\VID_2A03&PID_0042',
+    'USB\\VID_2341&PID_0210',
+    'USB\\VID_2341&PID_0242',
+    // For chinese clones that use CH340
+    'USB\\VID_1A86&PID_7523'
+];
+
+/**
+ * Configuration of serialport
+ * @readonly
+ */
+const SERIAL_CONFIG = {
+    baudRate: 115200,
+    dataBits: 8,
+    stopBits: 1
+};
+
+/**
+ * Configuration for arduino-cli.
+ * @readonly
+ */
+const DIVECE_OPT = {
+    type: 'arduino',
+    fqbn: 'arduino:avr:mega:cpu=atmega2560',
+    firmware: 'arduinoMega2560.hex'
+};
+/**
+ * Manage communication with a Arduino Uno peripheral over a OpenBlock Link client socket.
+ */
+class ScratchStar extends ArduinoPeripheral{
+    /**
+     * Construct a Arduino communication object.
+     * @param {Runtime} runtime - the OpenBlock runtime
+     * @param {string} deviceId - the id of the extension
+     * @param {string} originalDeviceId - the original id of the peripheral, like xxx_arduinoUno
+     */
+    constructor (runtime, deviceId, originalDeviceId) {
+        super(runtime, deviceId, originalDeviceId, PNPID_LIST, SERIAL_CONFIG, DIVECE_OPT);
+    }
+}
+
+/**
+ * OpenBlock blocks to interact with a Arduino Uno peripheral.
+ */
+class ScratchStarDevice {
+    
+    /**
+     * Construct a set of Arduino blocks.
+     * @param {Runtime} runtime - the OpenBlock runtime.
+     * @param {string} originalDeviceId - the original id of the peripheral, like xxx_arduinoUno
+     */
+    constructor (runtime, originalDeviceId) {
+        /**
+         * The OpenBlock runtime.
+         * @type {Runtime}
+         */
+        this.runtime = runtime;
+
+        // Create a new Arduino uno peripheral instance
+        this._peripheral = new ArduinoUno(this.runtime, this.DEVICE_ID, originalDeviceId);
+
+        this._peripheral.numDigitalPins = 14;
+    }
+
+    /**
+     * @returns {Array.<object>} metadata for this extension and its blocks.
+     */
+    getInfo () {
+        return [
+            {
+                id: 'pin',
+                name: 'Scratch Star',
+                color1: '#4C97FF',
+                color2: '#3373CC',
+                color3: '#3373CC',
+
+                blocks: [
+                    
+                ],
+                menus: {
+                    
+                }
+            }
+        ];
+    }
+
+}
+
+module.exports = ScratchStarDevice;
--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -53,7 +53,8 @@ const builtinDevices = {
     arduinoRaspberryPiPico: () => require('../devices/arduinoRaspberryPiPico/arduinoRaspberryPiPico'),
     // Microbit
     microbit: () => require('../devices/microbit/microbit'),
-    microbitV2: () => require('../devices/microbit/microbitV2')
+    microbitV2: () => require('../devices/microbit/microbitV2'),
+    scratchstar: () => require('../devices/scratchstar/scratchstar'),
 
     // TODO: transform these to device extension.
     // wedo2: () => require('../extensions/scratch3_wedo2'),
